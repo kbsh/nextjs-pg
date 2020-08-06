f4f1806a273f98c81f24667b2c2c4a38ee973adc
kbsh <batake0919@gmail.com>
2020-08-06 15:56:46 +0900

    :lipstick: ヘッダー、メニュー UI修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,17 @@
-import clsx from 'clsx';
 import Link from 'next/link';
 import React from 'react';
 
+import { SwipeableDrawer } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
 import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import { createStyles, makeStyles, Theme, useTheme } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import MenuIcon from '@material-ui/icons/Menu';
 import { routes } from '@utils/routes';
 
@@ -22,47 +19,18 @@ const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    appBar: {
-      transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-    },
-    appBarShift: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-      transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
+    list: {
+      width: drawerWidth,
     },
     menuButton: {
       marginRight: theme.spacing(2),
     },
-    hide: {
-      display: 'none',
-    },
-    drawer: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-    drawerPaper: {
-      width: drawerWidth,
-    },
-    drawerHeader: {
-      display: 'flex',
-      alignItems: 'center',
-      padding: theme.spacing(0, 1),
-      // necessary for content to be below app bar
-      ...theme.mixins.toolbar,
-      justifyContent: 'flex-end',
-    },
+    toolbar: theme.mixins.toolbar,
   })
 );
 
-export default function PersistentDrawerLeft() {
+export default function HeaderAndMenu() {
   const classes = useStyles();
-  const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
@@ -75,19 +43,14 @@ export default function PersistentDrawerLeft() {
 
   return (
     <React.Fragment>
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
+      <AppBar position="fixed">
         <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
             onClick={handleDrawerOpen}
             edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
+            className={classes.menuButton}
           >
             <MenuIcon />
           </IconButton>
@@ -96,22 +59,10 @@ export default function PersistentDrawerLeft() {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </div>
+      <SwipeableDrawer open={open} onOpen={handleDrawerOpen} onClose={handleDrawerClose}>
+        <div className={classes.toolbar} />
         <Divider />
-        <List>
+        <List className={classes.list}>
           {routes.map((route, index) => (
             <Link href={route.href} key={index}>
               <ListItem button>
@@ -123,7 +74,7 @@ export default function PersistentDrawerLeft() {
             </Link>
           ))}
         </List>
-      </Drawer>
+      </SwipeableDrawer>
     </React.Fragment>
   );
 }
